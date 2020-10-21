954251f2a2cb44e89f77ad7932407ed6be36149c
Alex Sobol <Maxvit-5@tut.by>
2020-10-21 11:57:53 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "YPImagePicker",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(name: "YPImagePicker", targets: ["YPImagePicker"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "YPImagePicker", dependencies: [], path: "Sources"),
+    ]
+)
